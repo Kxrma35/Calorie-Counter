9e738b68cd3b2c8d161554808a795bbc744c6af0
kxrma35 <karmanjeruh5@gmail.com>
2026-03-01 22:55:48 +0300

    Add Utility helpers

--- script.js
+++ script.js
@@ -15,6 +15,32 @@ const progressPath = document.getElementById('ringProgress');
 const themeToggle = document.getElementById('themeToggle');
 
 let items = loadItems();
-let dailyGoal = 2000; 
+let dailyGoal = 2000; //  default goal
+
+
+// Utility helpers
+function readCookie(name) {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? decodeURIComponent(match[2]) : null;
+}
+function setCookie(name, value, days = 7) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/; SameSite=Lax`;
+}
+function deleteCookie(name) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+}
+function formatTime(ts) {
+  const d = new Date(ts);
+  return d.toLocaleString();
+}
+function escapeHtml(s) {
+  return String(s)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
 
 
